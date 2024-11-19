5f57d32705eabf5e0adceb4e63ae4217b2860ee2
Yaneli Cel <yanelicelcouoh@gmail.com>
2024-11-19 16:30:01 -0600

    Dios helpme

--- config.js
+++ config.js
@@ -1,21 +1,25 @@
-// const { Sequelize } = require('sequelize');
-
-// const sequelize = new Sequelize('content_platform', 'root', 'Yaneli18062003', {
-//     host: 'localhost',
-//     dialect: 'mysql', 
-// });
-
-// module.exports = sequelize;
-
 const { Sequelize } = require('sequelize');
-
-// Cargar variables de entorno
-require('dotenv').config();
+require('dotenv').config(); // Cargar variables de entorno
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres', // Cambia a 'postgres'
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // Este parámetro es importante para las conexiones en Render
+        }
+    },
+    retry: {
+        max: 3, // Número de intentos
+        match: [
+            /ECONNRESET/,
+            /ETIMEDOUT/,
+            /ECONNREFUSED/
+        ]
+    }
 });
 
 module.exports = sequelize;
+
