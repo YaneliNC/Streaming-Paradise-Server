633a5e2ef45c4b6d8fcd77c9aa3d337aa08f7b43
Yaneli Cel <yanelicelcouoh@gmail.com>
2024-11-18 22:51:40 -0600

    DIOS AYUDA2

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('content_platform', 'root', 'Yaneli18062003', {
     host: 'localhost',
-    dialect: 'mysql', // O el dialecto que estés utilizando
+    dialect: 'mysql', 
 });
 
 module.exports = sequelize;
