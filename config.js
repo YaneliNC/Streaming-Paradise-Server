b1d896daaf29500aaa3989bfd6cb2fbc3fd50216
Yaneli Cel <yanelicelcouoh@gmail.com>
2024-11-19 15:45:35 -0600

    Actualizar configuración para PostgreSQL

--- config.js
+++ config.js
@@ -1,8 +1,21 @@
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize('content_platform', 'root', 'Yaneli18062003', {
+//     host: 'localhost',
+//     dialect: 'mysql', 
+// });
+
+// module.exports = sequelize;
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('content_platform', 'root', 'Yaneli18062003', {
-    host: 'localhost',
-    dialect: 'mysql', 
+// Cargar variables de entorno
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres', // Cambia a 'postgres'
 });
 
 module.exports = sequelize;
